2cb310a490437ced4288bae34d85c4a50c58dd08
Dephairto <dephairto@gmail.com>
2023-10-27 22:23:46 -0400

    Create Grid

---
+++ src/components/visual/grid.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import Tile from "./plantStates/tile";
+
+export function Grid(
+    { height, width, tileStates } : 
+    { height: number, width: number, tileStates: Array<Array<number>> }
+) {
+    let tiles: Array<React.ReactNode> = [];
+    for (let i = 0; i < height; i++) {
+        let row: Array<React.ReactNode> = [];
+        for (let j = 0; j < width; j++) {
+            row.push(<Tile tileState = { tileStates[i][j] } />)
+        }
+        tiles.push(<div className = "Row" children = { row } />);
+    }
+
+    return (
+        <div className = "Grid" >
+            { tiles }
+        </div>
+    );
+}
