8ca1bdedaf0f0ac1a7531bee7b46c2ab4e1524c2
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-10-27 21:51:00 -0400

    added tailwind

---
+++ src/components/visual/plantStates/empty.tsx
@@ -0,0 +1,6 @@
+export default function EmptyTile()
+{
+    return(
+        <div ></div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-
+    <div className='bg-black w-[200px] h-[100px]'>a</div>
   </React.StrictMode>
 );
