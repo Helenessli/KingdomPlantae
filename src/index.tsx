9e108dc72389d9a3bf9405b422de442a4565af5d
Helenessli <helenessli6@gmail.com>
2023-10-29 16:17:35 -0400

    add mainmenu

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import MainMenu from './components/ui/MainMenu';
 import App from './app';
 
 import './index.css';
@@ -11,6 +11,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MainMenu />
   </React.StrictMode>
 );
