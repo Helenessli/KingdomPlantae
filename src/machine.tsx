789fbf57976ee3065059465355a1a687d99bed42
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-10-28 21:57:42 -0400

    made machine implementable bla bla bla

--- src/machine.tsx
+++ src/machine.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
-const [headPos, setHeadpos] = useState([0, 0]);
+const tileStates:any = [];
+const monster:any = [];
+
+const gridHeight = 50;
+const gridWidth = 50;
+
+/* DO NOT COPY PASTE ANY CODE ABOVE THIS LINE */ 
+
 const [bodyLength, setBodyLength] = useState(1);
+
 
 const directionMove: any = 
 {
@@ -11,38 +19,37 @@ const directionMove: any =
   "left":  [-1, 0]
 }
 
-const maxHeight = 50;
-const maxWidth = 50;
-
 let arr:any = []
 
 function movementDecision ()
 {
   let dir = "unknown";
   let max = [0, dir];
-  if (bodyLength == 1)
+  if (monster.length == 1)
   {
     //check all 4 sides
-    dir = greedyDecision(["up", "down", "right", "left"], 4, arr);
+    dir = greedyDecision(["up", "down", "right", "left"], 4);
   }
   else{
     //check all 3 sides
-    dir = greedyDecision(["up", "right", "left"], 3, arr);
+    dir = greedyDecision(["up", "right", "left"], 3);
   }
   //update the movement and then update headposition
 
   let [x, y] = directionMove[dir];
-  setHeadpos([headPos[0] + x, headPos[1] + y]);
+  //updateHead(monster[0][0], monster[0][1]) //x and y
+  //setHeadpos([headPos[0] + x, headPos[1] + y]);
 }
 
-function greedyDecision(directions: any, len: number, arr: any)
+function greedyDecision(directions: any, len: number)
 {
-  let max = 0;
-  let max_det = 0;
+  const headPos = monster[0];
+  let max = 0; //max amount of food in straight line
+  let max_det = 0; //determines which dir if max food is same for two different dir
   let dir = "unknown";
   for (let i = 0; i < len; i++)
   {
-    let [food, det] = checkGrass(directions[i], arr);
+    let [food, det] = checkGrass(directions[i]);
     if (food > max)
     {
       max = food;
@@ -56,12 +63,12 @@ function greedyDecision(directions: any, len: number, arr: any)
     }
   }
 
-  if (dir == "unkown")
+  if (dir == "unkown") //if each dir has no food at all, randomize
   {
     dir = directions[Math.floor(Math.random() * len)];
     let x = headPos[0] + directionMove[dir][0];
     let y = headPos[1] + directionMove[dir][1];
-    while(x < 0 || x >= maxWidth || y < 0 || y >= maxHeight) //make it make sure its not out of bounds
+    while(x < 0 || x >= gridWidth || y < 0 || y >= gridHeight) //make it make sure its not out of bounds
     {
       dir = directions[Math.floor(Math.random() * len)];
       x = headPos[0] + directionMove[dir][0];
@@ -72,15 +79,16 @@ function greedyDecision(directions: any, len: number, arr: any)
   return dir;
 }
 
-function checkGrass(dir: String, arr: any) //checks for grass and returns num of grass it can eat, and in the area
+function checkGrass(dir: String) //checks for grass and returns num of grass it can eat, and in the area
 {
   let xi = 0;
   let yi = 0;
   let xf = 0;
   let yf = 0;
 
-  const x = headPos[0];
-  const y = headPos[1];
+  //head location
+  const x = monster[0][0];
+  const y = monster[0][1];
 
   let res = 0;
   let food = 0;
@@ -121,15 +129,15 @@ function checkGrass(dir: String, arr: any) //checks for grass and returns num of
   {
     for (let j = xi; j <= xf; j++)
     {
-      if ((y+i >= 0 && y+i < maxHeight) && (x+j >= 0 && x+j < maxWidth))
+      if ((y+i >= 0 && y+i < gridHeight) && (x+j >= 0 && x+j < gridWidth))
       {
         if (i == 0 || j == 0)
         {
-          food += arr[y+i][x+j];
+          food += tileStates[y+i][x+j] % 10;
         }
         else
         {
-          res += arr[y+i][x+j];
+          res += tileStates[y+i][x+j] % 10;
         }
       }
     }
