f58771c2d77462f2f30e3ea6e310b343374ba769
William Li <52115161+Trollermaner@users.noreply.github.com>
2023-10-28 21:12:41 -0400

    added machine.tsx

--- src/components/visual/tile.tsx
+++ src/components/visual/tile.tsx
@@ -3,16 +3,18 @@ let DirtTile = () => <div className =
 />;
 
 let SproutTile = () => <div className =
-  "w-[25px] h-[25px] bg-[#77ff00] hover:bg-[#000000] hover:cursor-pointer rounded-full"
+  "w-[25px] h-[25px] bg-[#77ff00] hover:bg-[#a0a0a0] hover:cursor-pointer rounded-full"
 />;
 
 let TeenTile = () => <div className = 
-  "w-[25px] h-[25px] bg-[#14911f] hover:bg-[#000000] hover:cursor-pointer rounded-full"
+  "w-[25px] h-[25px] bg-[#0d7416] hover:bg-[#a0a0a0] hover:cursor-pointer rounded-full"
 />;
 
-let AdultTile = () => <div className = 
-  "w-[25px] h-[25px] bg-[#ff00e6] hover:bg-[#000000] hover:cursor-pointer rounded-full"
-/>;
+let AdultTile = () => <div className="bg-[#ff0000]">
+  <div className = 
+  "w-[25px] h-[25px] bg-[#ffb61a] hover:bg-[#a0a0a0] hover:cursor-pointer rounded-full"
+/>
+</div>;
 
 let tileTypes  = [ DirtTile, SproutTile, TeenTile, AdultTile ];
 
