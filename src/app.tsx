ca11a34b9ef98e71ec9ba08313e127d312b48839
Dephairto <dephairto@gmail.com>
2023-10-29 13:42:40 -0400

    Add Razors, fix setTick feature

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { updateBoard } from "./components/gameLogic";
+import { useState } from "react";
 import Board from "./components/board";
 
 type DisplayData = number[][];
@@ -8,27 +7,23 @@ const updateTestCase: DisplayData = [];
 for (let i = 0; i < 25; i++) {
   updateTestCase.push([...Array(40)].map(() => Math.round(Math.random() * 0.55)));
 }
+updateTestCase[10][10] = 4;
+updateTestCase[11][10] = 14;
+updateTestCase[12][10] = 14;
+updateTestCase[13][10] = 14;
+updateTestCase[14][10] = 14;
+let initMachineData = [[10, 10], [11, 10], [12, 10], [13, 10], [14, 10], [15, 10]];
 
 export default function App() {
-  // const [tileStates, setTileStates] = useState(updateTestCase);
-  // const [tickIntervalMs, setTickIntervalMs] = useState(500);
-  // const [monster, setMonster]: any = useState([[0, 0]]); //x,y coordinates of each monster part
-  // const [lastDir, setLastDir] = useState("unknown");
-  // const [razor, setRazor]: any = useState([]); //3x3 boolean grid showing current razor configuration
-  // const [foodCnt, setFoodCnt]: any = useState(0); //resets to zero after reaching 20
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTileStates(updateTileStates(tileStates));
-  //     setMonster(updateBoard(monster, tileStates, setTileStates, updateTileStates, gridWidth, gridHeight, lastDir, setLastDir, foodCnt, setFoodCnt));
-  //   }, tickIntervalMs);
-  //   return () => clearInterval(interval);
-  // }, [tileStates]);
+  const [tickIntervalMs, setTickIntervalMs] = useState(500);
 
   return (
     <div className = "w-screen h-screen flex content-center justify-center flex-wrap" >
-      <Board initialData = { updateTestCase } />
-      {/* <button className="border-black border-2 rounded-xl w-14 h-10 mx-3"onClick={() => setTickIntervalMs((tickIntervalMs+800)%1200)}>{(tickIntervalMs === 100? "3x" : (tickIntervalMs === 500? "2x" : "1x"))}</button> */}
+      <Board initDisplayData = { updateTestCase } initMachineData = { initMachineData } tickInterval = { tickIntervalMs }  />
+      <button className = "border-black border-2 rounded-xl w-14 h-10 mx-3"
+        onClick={() => setTickIntervalMs((tickIntervalMs+800)%1200)}>
+          {(tickIntervalMs === 100? "3x" : (tickIntervalMs === 500? "2x" : "1x"))}
+        </button>
     </div>
   );
 }
--- src/logics/conway.tsx
+++ src/logics/conway.tsx
@@ -1,36 +1,59 @@
-type DisplayData = number[][];
+type tiles = number[][];
 
-export default function conway(displayData: DisplayData) {
-  let copy = [...displayData].map(row => [...row]);  // Hard copy here
+// to do
+// - only implement rules if is a plant
+// - update if previously was razor
+// -
+export default function conway(tiles: number[][]) {
+  let copy = [...tiles].map((row) => [...row]); // Hard copy here
 
-  let h = displayData.length;
-  let w = (displayData.length == 0) ? 0 : displayData[0].length;
+  let h = tiles.length;
+  let w = tiles.length == 0 ? 0 : tiles[0].length;
   for (let x = 0; x < h; x++) {
     for (let y = 0; y < w; y++) {
-      // Get number of neighbors for state 0, 1, 2, 3
-      let neighbors = [0, 0, 0, 0];
-      for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          if (0 <= x + i && x + i < h &&
-            0 <= y + j && y + j < w) {
-            neighbors[displayData[x + i][y + j]]++;
-          }
-        }
-      }
-
-      // Update tileStates based on number of neighbors
-      let numAlive = neighbors[1] + neighbors[2] + neighbors[3];
-      if (displayData[x][y] != 0) {
-        if (displayData[x][y] != 3 && (numAlive == 2 || numAlive == 3 || numAlive == 4)) {
-          copy[x][y] = displayData[x][y] + 1;
-        } else {
+      switch (tiles[x][y]) {
+        case 8:
           copy[x][y] = 0;
-        }
-      } else if (numAlive > 2) {
-        copy[x][y] = 1;
+          break;
+        case 9:
+          copy[x][y] = 10;
+          break;
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 14:
+          break;
+        default: // 0 1 2 3 10 11 12 13
+          let neighbors = [0, 0, 0, 0];
+          for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+              if (i == 0 && j == 0) continue;
+              if (0 <= x + i && x + i < h && 0 <= y + j && y + j < w) {
+                let type = tiles[x + i][y + j] % 10;
+                if (type <= 3) neighbors[type]++;
+              }
+            }
+          }
+          // Update tileStates based on number of neighbors
+          let numAlive = neighbors[1] + neighbors[2] + neighbors[3];
+          if (tiles[x][y] % 10 != 0) {
+            if (
+              tiles[x][y] % 10 != 3 &&
+              (numAlive == 2 || numAlive == 3 || numAlive == 4)
+            ) {
+              copy[x][y] = tiles[x][y] + 1;
+            } else {
+              if (tiles[x][y] >= 10) copy[x][y] = 10;
+              else copy[x][y] = 0;
+            }
+          } else if (numAlive > 2) {
+            if (tiles[x][y] >= 10) copy[x][y] = 11;
+            else copy[x][y] = 1;
+          }
+          break;
       }
     }
   }
-
   return copy;
 }
